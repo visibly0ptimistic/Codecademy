64b8a5c62a87110c5bea53359b0aae4eddc056eb
visibly0ptimistic <victorejiasi@yahoo.com>
2022-08-25 22:17:31 -0500

    Dictionaries

---
+++ Lessons/CS_Python/CS_101/Dictionaries/invalid_keys.py
@@ -0,0 +1,3 @@
+# children = {["Johannes", "Rosmarie", "Eleonore"]: "von Trapp", ["Sonny", "Fredo", "Michael"]: "Corleone"}
+
+children = {"von Trapp":["Johannes", "Rosmarie", "Eleonore"], "Corleone":["Sonny", "Fredo", "Michael"]}
