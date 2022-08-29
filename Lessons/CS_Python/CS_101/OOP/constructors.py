824d3cdeec1cdf12fbb943ceafcfe9f0315b5ba4
visibly0ptimistic <victorejiasi@yahoo.com>
2022-08-28 21:28:56 -0500

    Object Oriented Programming

---
+++ Lessons/CS_Python/CS_101/OOP/constructors.py
@@ -0,0 +1,8 @@
+class Circle:
+    pi = 3.14
+
+# Add constructor here:
+    def __init__(self, diameter):
+        print("New circle with diameter: {diameter}".format(diameter=diameter))
+    
+teaching_table = Circle(36)
---
+++ Lessons/CS_Python/CS_101/OOP/type.py
@@ -0,0 +1,8 @@
+a_string = "Cool String"
+an_int = 12
+
+print(type(a_string))
+# prints "<class 'str'>"
+
+print(type(an_int))
+# prints "<class 'int'>"
