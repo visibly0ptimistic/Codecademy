423d7a2c60a9ee2979ace7db88a8f574d50bf6b8
visibly0ptimistic <victorejiasi@yahoo.com>
2022-08-29 23:34:53 -0500

    Hash Maps

---
+++ Lessons/CS_Python/CS_102/Hash Maps/hashing_function.py
@@ -0,0 +1,9 @@
+class HashMap:
+    def __init__(self, array_size):
+        self.array_size = array_size
+        self.array = [None for item in range(array_size)]
+
+    def hash(self, key):
+        key_bytes = key.encode()
+        hash_code = sum(key_bytes)
+        return hash_code
