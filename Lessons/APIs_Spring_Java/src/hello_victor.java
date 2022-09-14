b59d954bd02efcfafdc9e3c26485090c4ba8bd0b
visibly0ptimistic <victorejiasi@yahoo.com>
2022-09-14 09:56:27 -0500

    Intro to Java

---
+++ Lessons/APIs_Spring_Java/src/hello_victor.java
@@ -0,0 +1,9 @@
+public class hello_victor {
+    public static void main
+(String[] args) {
+        System.out.println("Hello Victor!");
+
+        String Hello = "Hi Java!";
+        System.out.println(Hello);
+    }
+}
---
+++ Lessons/APIs_Spring_Java/src/intro_java.java
@@ -0,0 +1,5 @@
+public class intro_java {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
