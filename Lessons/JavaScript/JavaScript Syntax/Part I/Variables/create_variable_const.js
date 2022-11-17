9abf4fd0cabbe16502980084e17b4ddf4b1db8c2
visibly0ptimistic <victorejiasi@yahoo.com>
2022-11-17 11:07:24 -0600

    Variables

---
+++ Lessons/JavaScript/JavaScript Syntax/Part I/Variables/create_variable_const.js
@@ -0,0 +1,11 @@
+const entree = 'Enchiladas';
+
+console.log(entree);
+
+entree = 'Tacos'
+
+/* 
+This code throws the following error when you run your code:
+
+TypeError: Assignment to constant variable. 
+*/
