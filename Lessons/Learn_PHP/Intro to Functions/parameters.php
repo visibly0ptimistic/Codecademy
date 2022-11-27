e987e68467bae5874e715e939c40910fe003480a
visibly0ptimistic <victorejiasi@yahoo.com>
2022-11-26 19:15:47 -0600

    Intro to Functions

---
+++ Lessons/Learn_PHP/Intro to Functions/parameters.php
@@ -0,0 +1,20 @@
+<?php
+// Write your code below:
+function increaseEnthusiasm
+($str_param)
+{
+    return $str_param . "!";
+}
+
+function repeatThreeTimes
+($str_param)
+{
+    return $str_param .
+$str_param . $str_param;
+}
+
+echo increaseEnthusiasm("oh");
+echo repeatThreeTimes("wow");
+
+echo increaseEnthusiasm
+(repeatThreeTimes("hai"));
