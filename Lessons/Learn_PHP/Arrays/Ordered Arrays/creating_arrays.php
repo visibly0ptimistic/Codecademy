887280b5e5d1a91f286a702e20bb595baeb99796
visibly0ptimistic <victorejiasi@yahoo.com>
2022-11-28 08:04:08 -0600

    Arrays

---
+++ Lessons/Learn_PHP/Arrays/Ordered Arrays/creating_arrays.php
@@ -0,0 +1,5 @@
+<?php
+// Write your code below:
+$first_array = array("hello", 88, "my", -12361.11, "friend");
+
+echo count($first_array);
---
+++ Lessons/Learn_PHP/Arrays/Ordered Arrays/pushing_popping.php
@@ -0,0 +1,15 @@
+<?php
+namespace Codecademy;
+
+$stack = ["wild success", "failure", "struggle"];
+// Write your code below:
+array_push($stack, "blocker","impediment");
+
+print_r($stack);
+
+array_pop($stack);
+array_pop($stack);
+array_pop($stack);
+array_pop($stack);
+
+print_r($stack);
