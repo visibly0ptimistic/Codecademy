718c4b75cbeccc76137b7870f5631f9a24a2112c
visibly0ptimistic <victorejiasi@yahoo.com>
2022-12-25 12:19:18 -0600

    Loops

---
+++ Lessons/JavaScript/JavaScript Syntax/Part II/Loops/repeating_tasks_manually.js
@@ -0,0 +1,11 @@
+// manual repeat
+const vacationSpots = ['Mozambique', 'Thailand', 'Bolivia'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+// loop
+for (let i = 0; i < vacationSpots.length; i++) {
+    console.log(vacationSpots[i]);
+}
