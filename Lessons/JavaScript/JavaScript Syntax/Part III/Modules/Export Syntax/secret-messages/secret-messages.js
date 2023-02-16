26a12aab910600a074d5d00a38d3c031b7efec09
visibly0ptimistic <victorejiasi@yahoo.com>
2023-02-16 15:49:31 -0600

    Modules

---
+++ Lessons/JavaScript/JavaScript Syntax/Part III/Modules/Export Syntax/secret-messages/secret-messages.js
@@ -0,0 +1,10 @@
+/* secret-messages.js */
+import { toggleHiddenElement, changeToFunkyColor } from '../modules/dom-functions.js';
+ 
+const buttonElement = document.getElementById('secret-button');
+const pElement = document.getElementById('secret-p');
+ 
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+  changeToFunkyColor(buttonElement);
+});
